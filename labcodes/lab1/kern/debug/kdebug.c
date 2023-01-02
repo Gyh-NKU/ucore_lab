c88843b916902f790ece53a18469699d89e6a369
青空 <guoyunhe1@outlook.com>
2023-01-02 19:48:02 +0800

    完成lab1：输出函数调用栈和寄存器状态，实现中断的处理

--- labcodes/lab1/kern/debug/kdebug.c
+++ labcodes/lab1/kern/debug/kdebug.c
@@ -302,5 +302,22 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+    uint32_t ebp = read_ebp();
+    uint32_t eip = read_eip();
+    int i, j;
+    for (i = 0; i < STACKFRAME_DEPTH; i ++) {
+        cprintf("ebp: %08x eip: %08x args", ebp, eip);
+        uint32_t *args = (uint32_t *)ebp + 2;
+        for (j = 0; j < 4; j ++) {
+            cprintf("0x%08x ", args[j]);
+        }
+        cprintf("\n");
+        print_debuginfo(eip - 1);
+        if (ebp == 0) {
+            break;
+        }
+        eip = *((uint32_t *)ebp + 1);
+        ebp = *((uint32_t *)ebp);
+    }
 }
 
