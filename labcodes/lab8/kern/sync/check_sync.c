63c2bff3cc8a7ecae307ddd66fb82d771eb4b2f3
青空 <guoyunhe1@outlook.com>
2023-01-11 20:40:20 +0800

    完成lab8

--- labcodes/lab8/kern/sync/check_sync.c
+++ labcodes/lab8/kern/sync/check_sync.c
@@ -105,7 +105,7 @@ int philosopher_using_semaphore(void * arg) /* i：哲学家号码，从0到N-1 
 
 struct proc_struct *philosopher_proc_condvar[N]; // N philosopher
 int state_condvar[N];                            // the philosopher's state: EATING, HUNGARY, THINKING  
-monitor_t mt, *mtp=&mt;                          // monitor
+monitor_t mt, *mtp=&mt;                                    // mp is mutex semaphore for monitor's procedures
 
 void phi_test_condvar (i) { 
     if(state_condvar[i]==HUNGRY&&state_condvar[LEFT]!=EATING
@@ -124,6 +124,14 @@ void phi_take_forks_condvar(int i) {
      // LAB7 EXERCISE1: YOUR CODE
      // I am hungry
      // try to get fork
+      // I am hungry
+      state_condvar[i]=HUNGRY; 
+      // try to get fork
+      phi_test_condvar(i); 
+      while (state_condvar[i] != EATING) {
+          cprintf("phi_take_forks_condvar: %d didn't get fork and will wait\n",i);
+          cond_wait(&mtp->cv[i]);
+      }
 //--------leave routine in monitor--------------
       if(mtp->next_count>0)
          up(&(mtp->next));
@@ -138,6 +146,11 @@ void phi_put_forks_condvar(int i) {
      // LAB7 EXERCISE1: YOUR CODE
      // I ate over
      // test left and right neighbors
+      // I ate over 
+      state_condvar[i]=THINKING;
+      // test left and right neighbors
+      phi_test_condvar(LEFT);
+      phi_test_condvar(RIGHT);
 //--------leave routine in monitor--------------
      if(mtp->next_count>0)
         up(&(mtp->next));
